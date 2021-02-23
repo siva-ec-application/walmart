1115a39a1c0fb069016e2f2d8dc81c9d09bce3a3
Siva Prasad G <sivagangavarapu26@gmail.com>
2021-02-23 18:10:43 +0530

    first commit

---
+++ DBUtilities.java
@@ -0,0 +1,2 @@
+echo"welcome to Git Repository Management"
+echo "git is Source Code Management Tool"
---
+++ Strings.sh
@@ -0,0 +1,5 @@
+echo"welcome to Strings file"
+
+echo"Madam,Madan,Manoj,Madhusudhan"
+
+echo"String1,String2,String3"
